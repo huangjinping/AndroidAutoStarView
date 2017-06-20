1d51f1deab35f74d99f86849da1d360bac9d986e
huangjinping <huangjinping1000@163.com>
2017-06-20 11:37:01 +0800

    utils

---
+++ autostarview/src/main/java/com/hjp/autostartview/Utils.java
@@ -0,0 +1,13 @@
+package com.hjp.autostartview;
+
+/**
+ * author Created by harrishuang on 2017/6/20.
+ * email : huangjinping@hdfex.com
+ */
+
+public class Utils {
+
+    public static String getVersion() {
+        return "1.0.0";
+    }
+}
